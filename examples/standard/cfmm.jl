1c9f4b4ffebc85b2a37979b2b46fcc4339d5b0d8
Theo Diamandis <tjdiamandis@gmail.com>
2024-05-22 18:25:08 -0400

    add cfmm example

---
+++ examples/standard/cfmm.jl
@@ -0,0 +1,102 @@
+#=
+# Market Clearing
+This example uses `ConvexFlows` to solve a market clearing problem
+
+
+
+```math
+    \ell(w) = \alpha \left(\log(1 + \exp(\beta w)) - \log 2\right) - 2w.
+```
+
+The output of a line is then $h(w) = w - \ell(w)$. The optimal power flow 
+problem is then to minimize the total cost (maximize its negative) subject to
+the power flow constraints and net flow constraint:
+
+```math
+\begin{aligned}
+    & \text{maximize} && -\sum_{i=1}^n (d_i - y_i)_+^2 \\
+    & \text{subject to} &&  y = \sum_{i=1}^n A_i x_i \\
+    &&& h(-(x_i)_1) \le (x_i)_2 ~ \text{for} i = 1, \dots, m. \\
+\end{aligned}
+```
+=#
+
+using ConvexFlows
+using Random, LinearAlgebra, SparseArrays
+using Plots, Graphs
+import GraphPlot
+import Cairo
+
+
+#=
+## Construct the problem
+
+First, we build a graph with 3 nodes (assets) and 3 edges (markets).
+=#
+
+n = 3
+edge_inds = [
+    (1, 2),
+    (1, 3),
+    (2, 3),
+]
+Adj = spzeros(Bool, n, n)
+for (i, j) in edge_inds
+    Adj[i, j] = true
+    Adj[j, i] = true
+end
+
+## Uniswap v2 trading function
+Random.seed!(1)
+Rs = [10*rand(2) for _ in 1:length(edge_inds)]
+f(δ, R1, R2) = R2*δ/(R1 + δ)
+
+graph = GraphPlot.gplot(
+    Graph(Adj),
+    nodefillc="black",
+    edgestrokec="dark gray",
+    edgelabel=[
+        "$(round(Rs[i][1], digits=2)), $(round(Rs[i][2], digits=2))" 
+        for i in 1:length(edge_inds)
+    ],
+    edgelabelc="white",
+)
+
+
+cfmms = Edge[]
+for (i, inds) in enumerate(edge_inds)
+    i1, i2 = inds
+    push!(cfmms, Edge((i1, i2); h=δ->f(δ, Rs[i][1], Rs[i][2]), ub=1e6))
+    push!(cfmms, Edge((i2, i1); h=δ->f(δ, Rs[i][2], Rs[i][1]), ub=1e6))
+end
+
+#=
+Next, we define a linear objective function that says we value each asset equally.
+=#
+
+## Objective function
+c = ones(n)
+obj = Linear(c);
+
+
+#=
+## Solve the problem
+=#
+prob = problem(obj=obj, edges=cfmms)
+result = solve!(prob; options=BFGSOptions(max_iters=12, print_iter=1))
+
+
+#=
+## Show the results
+
+Some nodes with demand 0 are generator nodes, which we see have a net outflow.
+=#
+
+println("Objective value: $(result.obj_val)")
+println("Netflows: $(prob.y)")
+for (i, x) in enumerate(prob.xs)
+    iszero(x) && continue
+    edge_ind = (i-1) ÷ 2 + 1
+    edge = isodd(i) ? edge_inds[edge_ind] : reverse(edge_inds[edge_ind])
+    println("$edge has flow $x")
+end
